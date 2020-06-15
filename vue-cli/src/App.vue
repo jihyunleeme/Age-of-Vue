503312f18b9c8bf947c95f0dfaea9080306c3580
jihyunleeme <jihyunlee.me@gmail.com>
2020-06-15 19:08:19 +0900

    Age-of-Vue.js 강의 실습파일 / initial commit

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 컴포넌트 명명법 종류 -->
+    <!-- 
+      <hello-world></hello-world> 케밥케이스
+    -->
+    <!-- 
+      <HelloWorld></HelloWorld> 파스칼케이스
+    -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
+  name: 'App',
+  components: {
+    HelloWorld
+    // 15번째줄 'hello-world': HelloWorld,와 같음
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/src/components/a.vue
@@ -0,0 +1,29 @@
+<!--
+var appHeader = {
+    template: '<div>header</div>',
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+-->
+
+<template>
+    <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+        methods: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
